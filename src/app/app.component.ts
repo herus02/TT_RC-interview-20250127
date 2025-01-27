2854aa879a79eb398a5587b5f5d473d3abb6ac6e
Herus Armstrong <herus02@gmail.com>
2025-01-26 21:34:56 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'TT_RC-interview-20250127';
+}
